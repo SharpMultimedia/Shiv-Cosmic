f147b77777e6b48c1a487f6dc8d3e063ea666de0
Onkar Auti <onkarauti1@gmail.com>
2024-05-10 17:20:41 +0530

    new data added

--- Shiv_Cosmic/asgi.py
+++ Shiv_Cosmic/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for astro project.
+ASGI config for Shiv_Cosmic project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shiv_Cosmic.settings')
 
 application = get_asgi_application()
--- Shiv_Cosmic/wsgi.py
+++ Shiv_Cosmic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for astro project.
+WSGI config for Shiv_Cosmic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'astro.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Shiv_Cosmic.settings')
 
 application = get_wsgi_application()
