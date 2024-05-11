031e0d8f0d9fd56ab8feaaf4bb71abb3d8993274
Onkar Auti <onkarauti1@gmail.com>
2024-05-11 18:41:30 +0530

    mail integration

--- Shiv_Cosmic/settings.py
+++ Shiv_Cosmic/settings.py
@@ -133,3 +133,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'developer.sharpmultimedia@gmail.com'
+EMAIL_HOST_PASSWORD = 'zoihbgrikcdiwxkj'
+EMAIL_USE_TLS = True
