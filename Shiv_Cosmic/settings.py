eb0a25dd164ea44ae89a56610361b83f4785c6f5
Onkar Auti <onkarauti1@gmail.com>
2024-07-16 16:20:49 +0530

    pg error resolve

--- Shiv_Cosmic/settings.py
+++ Shiv_Cosmic/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'main.middleware.ReferrerPolicyMiddleware',
 ]
 
 ROOT_URLCONF = 'Shiv_Cosmic.urls'
