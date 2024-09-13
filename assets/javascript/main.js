7cdc5925968e8f558436e2f684850e26c22ed6cc
root <root@srv541047.hstgr.cloud>
2024-09-13 13:56:34 +0000

    server push

---
+++ assets/javascript/main.js
@@ -0,0 +1,57 @@
+// testimonial slider 
+
+var swiper = new Swiper(".testimonial-slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    speed: 9000,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        1024: {
+            slidesPerView: 2,
+            spaceBetween: 20
+        },
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 20
+        }
+    }
+});
+
+
+// Navigation 
+
+function Menu(e) {
+    let list = document.querySelector('ul');
+    if (e.querySelector('i').classList.contains('fa-bars')) {
+        e.querySelector('i').classList.remove('fa-bars');
+        e.querySelector('i').classList.add('fa-times');
+        list.classList.add('open');
+        list.style.position = 'absolute';
+    } else {
+        closeMenu(); // Calls the function to close the menu
+    }
+}
+
+function closeMenu() {
+    let list = document.querySelector('ul');
+    let menuIcon = document.querySelector('.fa-times');
+    if (menuIcon) {
+        menuIcon.classList.remove('fa-times');
+        menuIcon.classList.add('fa-bars');
+    }
+    list.classList.remove('open');
+}
+
+
+
