7020e618614df96e2adeb80e32b989633bd1f614
prajwal <prajwalwarkhade2862000@gmail.com>
2025-06-25 11:06:32 +0530

    models changes

--- main/models.py
+++ main/models.py
@@ -36,8 +36,8 @@ class AstroBooking(models.Model):
     email = models.EmailField()  
     paid = models.BooleanField(default=False)  
 
-    def __str__(self):  
-        return f"{self.first_name} {self.last_name}"          
+    def __str__(self):
+        return self.name        
     
 class Report(models.Model):
     name = models.CharField(max_length=255)
